07a607967af378e4503188a056fa218f355af57d
Garavirod <rodrigogarciaavila26@gmail.com>
2019-12-18 16:27:36 -0600

    Concurrently script

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 const path = require('path'); //Nos permite acceder a los archvios que tenemos loccalmente
 const webpack = require('webpack');
 
-nodule.exports={
+module.exports = {
     entry: './public/js/app.js',
     output: {
-        filename:'bundle.js', //Archivo de salida
-        path: path.join(__dirname,"./public/dist") //Creamos una nueva carptea
+        filename: 'bundle.js', //Archivo de salida
+        path: path.join(__dirname, "./public/dist") //Creamos una nueva carptea
     },
-    module:{
-        rules:[
-            {
-                test: /\.m?js$/, //Busca todos los archovos js en la carpeta designada
-                use :{
-                    loader: 'babel-loader',//Indicamos que puglin queremos utilizar
-                    options:{
-                        presets :['@babel/preset-env']
-                    }
+    module: {
+        rules: [{
+            test: /\.m?js$/, //Busca todos los archovos js en la carpeta designada
+            use: {
+                loader: 'babel-loader', //Indicamos que puglin queremos utilizar
+                options: {
+                    presets: ['@babel/preset-env']
                 }
             }
-        ]
+        }]
     }
 }
