1db928b73536fad5f18d14e6f9ff29b13010e51e
Garavirod <rodrigogarciaavila26@gmail.com>
2019-12-18 18:31:04 -0600

    Registro del lado del front

--- public/js/modulos/proyectos.js
+++ public/js/modulos/proyectos.js
@@ -1,29 +1,42 @@
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import { exportDefaultDeclaration } from 'babel-types';
 
 const btnEliminar = document.querySelector('#eliminar-proyecto');
-btnEliminar.addEventListener('click', () => {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.value) {
-            Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            );
-            // Redirect to user to home
-            setTimeout(() => {
-                window.location.href = '/'
-            }, 3000);
+if (btnEliminar) {
+    btnEliminar.addEventListener('click', (e) => {
+        // Accedemos a los atributos personalizados
+        const urlProyecto = e.target.dataset.proyectoUrl;
+        // console.log(urlProyecto);
+        // return;
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            // On this block we'll send a petiton to axios
+            const url = `${location.origin}/proyectos/${urlProyecto}`;
+            axios.delete(url, { params: urlProyecto }).then(function(respuesta) { console.log(respuesta) });
+            return;
+            if (result.value) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                );
+                // Redirect to user to home
+                setTimeout(() => {
+                    window.location.href = '/'
+                }, 3000);
 
 
-        }
-    })
-});
+            }
+        })
+    });
+}
+
+export default btnEliminar;
